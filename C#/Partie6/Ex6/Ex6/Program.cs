bc5c2c04a77cfb3b64083bcd343da3ed64158de6
c-bringer <corentin.bringer@limayrac.fr>
2021-06-01 15:08:26 +0200

    part6 ex6

---
+++ C#/Partie6/Ex6/Ex6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> peches = new List<String>();
+
+            peches.Add("Peche1");
+            peches.Add("Peche2");
+            peches.Add("Peche3");
+            peches.Add("Peche4");
+            peches.Add("Peche5");
+            peches.Add("Peche6");
+            peches.Add("Peche7");
+
+            foreach(String peche in peches)
+            {
+                Console.WriteLine("{0}", peche);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
