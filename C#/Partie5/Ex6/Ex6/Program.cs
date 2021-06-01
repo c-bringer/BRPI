d2b48f64a88bc64d0ffb1163804f0f9cc1f7bcdf
c-bringer <corentin.bringer@limayrac.fr>
2021-06-01 09:11:51 +0200

    init ex6 part5

---
+++ C#/Partie5/Ex6/Ex6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo info;
+
+            do
+            {
+                Console.WriteLine("Veuillez taper une lettre, svp");
+                info = Console.ReadKey(true);
+            } while (info.Key != ConsoleKey.X);
+        }
+    }
+}
